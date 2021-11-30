73f5bb73cf1f0350c927a84503e3658713fa480d
lklao <lklao@dsej.gov.mo>
2021-11-30 19:16:39 +0800

    testing

--- scripts/main.js
+++ scripts/main.js
@@ -15,7 +15,7 @@ let myHeading = document.querySelector('h1');
 function setUserName() {
 	let myName = prompt('Please enter your name.');
 	localStorage.setItem('name', myName);
-	myHeading.textContent = 'Mozilla is cool, ' + myName + '!!' ;
+	myHeading.textContent = 'Mozilla is cool, ' + myName + '!!!' ;
 }
 
 function setUserName() {
